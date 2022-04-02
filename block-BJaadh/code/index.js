f3408145dae79ff0f63becd3ac115df81f769af7
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-04-02 14:40:44 +0530

    methods

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -11,37 +11,51 @@ Clone the array before using sort method: [...arr]
 */
 
 // - Find the index of `101` in numbers
-
+// console.log(numbers.indexOf(101))
 // - Find the last index of `9` in numbers
-
+// console.log(numbers.lastIndexOf(9))
 // - Convert value of strings array into a sentance like "This is a collection of words"
-
+let one = strings.reduce((acc, word) => {
+  return acc + " " + word
+}, " ")
+let two = strings.join(" ")
 // - Add two new words in the strings array "called" and "sentance"
+let newStringArray = [...strings]
+newStringArray.push('called', 'sentence')
 
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
-
+newStringArray.join(" ")
 // - Remove the first word in the array (strings)
-
+// newStringArray.shift()
 // - Find all the words that contain 'is' use string method 'includes'
-
-// - Find all the words that contain 'is' use string method 'indexOf'
-
+let filetrArr = newStringArray.filter((word) => word.includes('is'))
+// - Find index of the words that contain 'is' use string method 'indexOf'
+newStringArray.forEach((word, index) => {
+  // if (word.includes('is')) console.log(index)
+})
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
-
+let res = numbers.every((num) => num % 3 == 0)
 // -  Sort Array from smallest to largest
-
+let newNumberArray = [...numbers]
+newNumberArray.sort((a, b) => a - b)
 // - Remove the last word in strings
-
+newStringArray.pop()
 // - Find largest number in numbers
-
+let max = numbers[0]
+numbers.forEach((num) => {
+  if (num > max) max = num
+})
 // - Find longest string in strings
-
+let long = ''
+strings.forEach((word) => {
+  if (word.length > long.length) long = word
+})
 // - Find all the even numbers
-
+res = numbers.filter((num) => num % 2 == 0)
 // - Find all the odd numbers
-
+res = numbers.filter((num) => num % 2 != 0)
 // - Place a new word at the start of the array use (unshift)
-
+strings.unshift('New')
 // - Make a subset of numbers array [18,9,7,11]
 
 // - Make a subset of strings array ['a','collection']
@@ -60,11 +74,21 @@ var customers = [
   { firstname: 'Jack', lastname: 'White' },
 ];
 // - Find all customers whose firstname starts with 'J'
-
+customers.forEach((person) => {
+  if (person.firstname.startsWith('J')) console.log(person.firstname + ' ' + person.lastname)
+})
 // - Create new array with only first name
-
+res = customers.map((person) => person.firstname)
 // - Create new array with all the full names (ex: "Joe Blogs")
-
+res = customers.map((person) => person.firstname + ' ' + person.lastname)
 // - Sort the array created above alphabetically
-
+res.sort()
 // - Create a new array that contains only user who has at least one vowel in the firstname.
+// vowel = [`a`,`e`,`i`,`o`,`u`]
+res = customers.filter((person) => {
+  if (person.firstname.includes(`a`)) return person.firstname
+  if (person.firstname.includes(`e`)) return person.firstname
+  if (person.firstname.includes(`i`)) return person.firstname
+  if (person.firstname.includes(`o`)) return person.firstname
+  if (person.firstname.includes(`u`)) return person.firstname
+})
